f4c7c89c089187f3ac8c1025d29db3ed34ddca48
Naver <naver@AL01200737.local>
2017-03-01 19:02:56 +0900

    [EOMJURONG] Bad Version - 회원가입 요청을 받으면 응답하는 기능 구현
    - index.html 을 요청하면 webapp아래에서 파일을 읽어 클라이언트로 보내준다.
    - post방식으로 회원가입이 요청되면 user 객체에 저장한다.
    - user 객체 저장 후, 302로 응답을 보내어 클라이언트가 index.html을 다시 요청하도록 한다.

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -8,6 +9,30 @@ import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 
 public class HttpRequestUtils {
+	
+	/**
+	 * Request 요청의 헤더 정보를 파싱한다.
+	 * @param requestInfo
+	 * @return
+	 */
+	public static Map<String, String> parseRequestInfo(String requestInfo) {
+		if (Strings.isNullOrEmpty(requestInfo)) {
+			return null;
+		}
+
+		String [] tokens = requestInfo.split(" ");
+		if (tokens.length != 3) {
+			return null;
+		}
+
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("method", tokens[0]);
+		result.put("url", tokens[1]);
+		result.put("version", tokens[2]);
+		
+		return result;
+	}
+	
     /**
      * @param queryString은
      *            URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,26 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
+import util.HttpRequestUtils.Pair;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,11 +36,42 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "This is Eomjurong Server!! ~H E L L O~".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            // request 정보 리딩
+        	BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        	String line = reader.readLine();
+        	if (line == null) {
+        		return;
+        	}
+
+        	// request 정보 파싱
+        	String url = HttpRequestUtils.parseRequestInfo(line).get("url");
+        	Map<String, String> params = null;
+
+        	int contentLength = 0;
+        	while (!"".equals(line)) {
+        		log.info(line);
+        		line = reader.readLine();
+        		if (line.contains("Content-Length")) {
+        			contentLength = Integer.parseInt(line.split(":")[1].trim());
+        		}
+        	}
+        	
+        	DataOutputStream dos = new DataOutputStream(out);
+        	byte[] body = null;
+
+        	if ("/user/create".equals(url)) {
+        		params = HttpRequestUtils.parseQueryString(IOUtils.readData(reader, contentLength));
+        		url = "/index.html";
+
+        		User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+        		log.info(user.toString());
+        		response302Header(dos, url);
+        	} else {
+        		body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        		response200Header(dos, body.length);
+        	}
+        	responseBody(dos, body);
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
@@ -43,7 +87,16 @@ public class RequestHandler extends Thread {
             log.error(e.getMessage());
         }
     }
-
+    
+    private void response302Header(DataOutputStream dos, String location) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 FOUND \r\n");
+            dos.writeBytes("Location: " + location);
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
     private void responseBody(DataOutputStream dos, byte[] body) {
         try {
             dos.write(body, 0, body.length);
